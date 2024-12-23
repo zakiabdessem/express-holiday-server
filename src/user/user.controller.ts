9bde627a100e057e1d799de03ac556b537c02dba
Zakilolo2 <zakiabdessemed35@gmail.com>
2024-12-23 22:23:03 +0100

    HOTFIX register client

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -108,7 +108,7 @@ export class UserController {
       console.error(error);
       return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
         statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-        message: 'Internal server error',
+        message: error.message || 'Internal server error',
       });
     }
   }
