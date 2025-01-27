9956bf43099a49525529c68ae0ec78ddd608f54f
Zakilolo2 <zakiabdessemed35@gmail.com>
2025-01-27 13:09:35 +0100

    Added Ticket by roles

--- src/statistics/statistics.controller.ts
+++ src/statistics/statistics.controller.ts
@@ -1,4 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { UserRole } from 'src/decorator/role.entity';
+import { Roles } from 'src/decorator/roles.decorator';
+import { GQLRolesGuard } from 'src/guard/gql-role.guard';
 import { TicketService } from 'src/ticket/ticket.service';
 
 @Controller('stats')
@@ -6,6 +9,8 @@ export class StatsController {
   constructor(private readonly ticketService: TicketService) {}
 
   @Get('tickets')
+  @Roles(UserRole.ADMIN, UserRole.SUPERAGENT)
+  @UseGuards(GQLRolesGuard)
   async getTicketStatistics() {
     const statusCounts = await this.ticketService.countTicketsByStatus();
     return statusCounts;
